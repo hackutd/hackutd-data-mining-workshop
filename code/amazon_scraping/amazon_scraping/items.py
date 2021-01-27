7ff17d80ba7004f742a13bcdbe26d8ededa5afa3
Ishpreet Bhasin <ishpreet2000@gmail.com>
2021-01-27 13:33:01 -0600

    Scrapy items

---
+++ code/amazon_scraping/amazon_scraping/items.py
@@ -0,0 +1,18 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.org/en/latest/topics/items.html
+
+
+# Import Item and Field types from scrapy
+from scrapy import Item, Field
+
+
+# TODO (2.1) Define ProductItem class attributes
+class ProductItem(Item):
+	pass
+
+
+# TODO (2.2) Define ReviewItem class attributes
+class ReviewItem(Item):
+	pass
